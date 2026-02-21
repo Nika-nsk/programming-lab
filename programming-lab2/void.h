984225f2c60daed57e879183b29f7acabb4039ed
Nika-nsk <viranesa999@gmail.com>
2026-02-21 08:42:19 +0700

    Добавили лабу 2

---
+++ programming-lab2/void.h
@@ -0,0 +1,17 @@
+#define N 10
+struct Flight {
+    int flight_number;
+    char destination[50];
+    int time;
+    float price;
+};
+
+
+typedef int (*cmp_func)(struct Flight, struct Flight);
+
+void sort(struct Flight mass[], int n, cmp_func check);
+void Print_mass(struct Flight mass[], int n);
+
+
+int byTime(struct Flight a, struct Flight b);
+int byPrice(struct Flight a, struct Flight b);
